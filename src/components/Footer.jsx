357e305bb439e5df6b3d0f385179530f056d2d75
idrisiayaz <idrisiayaz@gmail.com>
2022-01-18 05:21:58 +0530

    keeperApp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+      <p>
+        Created by{" "}
+        <a
+          href="https://idrisiayaz.github.io/Ayaz-idrisi"
+          style={{ color: "grey" }}
+        >
+          @idrisiayaz
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
